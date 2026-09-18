dcdf1cee14ef442f57716d7c57a71f6ed6ae2687
Maryam Ateeq <maryamateeqc@gmail.com>
2026-09-18 23:32:00 +0500

    Complete Task 5

--- task5.cpp
+++ task5.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 bool isSorted(const int* arr, const int size);
+bool isSorted(const int* arr, const int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
 void testSortedArray()
 {
     int arr[] = {1, 2, 3, 4, 5};
@@ -70,4 +82,19 @@ void testAllEqual()
     bool result = isSorted(arr, 5);
 
     assert(result == true);
+}
+int main()
+{
+    testSortedArray();
+    testUnsortedArray();
+    testDuplicateValues();
+    testSingleElement();
+    testDescendingArray();
+    testNegativeValues();
+    testTwoElements();
+    testAllEqual();
+
+    cout << "All tests passed!" << endl;
+
+    return 0;
 }
